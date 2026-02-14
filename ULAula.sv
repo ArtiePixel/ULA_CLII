3c49fd26d908599db933492636aa2afb4e9fbf2d
Artie Gomes <155239726+ArtiePixel@users.noreply.github.com>
2026-02-14 13:08:28 -0300

    refactor: ULAula module using logic types

--- ULAula.sv
+++ ULAula.sv
@@ -1,51 +1,50 @@
 module ULAula(
-  input  [3:0] S,
-  input  [4:0] A, B,
-  input        R,
-  output reg [4:0] O,
-  output reg       Cout,
-  output reg       Zero
+  input  logic [3:0] S,
+  input  logic [5:0] A, B,
+  input  logic       R,
+  output logic [5:0] O,
+  output logic       Cout,
+  output logic       Zero
 );
 
-  reg [5:0] tmp;
+  logic [6:0] tmp;
 
-  always @(*) begin
-    
-    tmp  = 6'd0;
-    O    = 5'd0;
+  always_comb begin
+    tmp  = '0;
+    O    = '0;
     Cout = 1'b0;
-    Zero = 1'b0; 
+    Zero = 1'b0;
 
-    case (S)
-      4'b0000: tmp = A + B;
-      4'b0001: tmp = A - B;
-      4'b0010: tmp = A + ~B;
-      4'b0011: tmp = A - ~B;
-      4'b0100: tmp = A + 1'b1; 
-      4'b0101: tmp = A - 1'b1;
-      4'b0110: tmp = B + 1'b1;
-      4'b0111: tmp = B - 1'b1;
-      4'b1000: tmp = {1'b0, (A & B)};
-      4'b1001: tmp = {1'b0, (~A)};
-      4'b1010: tmp = {1'b0, (~B)};
-      4'b1011: tmp = {1'b0, (A | B)};
-      4'b1100: tmp = {1'b0, (A ^ B)};
-      4'b1101: tmp = {1'b0, ~(A & B)};
-      4'b1110: tmp = {1'b0, A};
-      4'b1111: tmp = {1'b0, B};
-      default: tmp = 6'd0;
-    endcase
-    
-    
     if (R) begin
-        O    = tmp[4:0];
-        Cout = tmp[5];
-        Zero = (O == 5'd0);
-    end
+      O    = '0;
+      Cout = 1'b0;
+      Zero = 1'b0;
+    end 
     else begin
-        O    = 5'b0; 
-        Cout = 1'b0; 
-        Zero = 1'b0; 
+      case (S)
+        4'b0000: tmp = {1'b0, A} + {1'b0, B};
+        4'b0001: tmp = {1'b0, A} - {1'b0, B};
+        4'b0010: tmp = {1'b0, A} + {1'b0, ~B};
+        4'b0011: tmp = {1'b0, A} - {1'b0, ~B};
+        4'b0100: tmp = {1'b0, A} + 7'd1;
+        4'b0101: tmp = {1'b0, A} - 7'd1;
+        4'b0110: tmp = {1'b0, B} + 7'd1;
+        4'b0111: tmp = {1'b0, B} - 7'd1;
+        4'b1000: tmp = {1'b0, (A & B)};
+        4'b1001: tmp = {1'b0, (~A)};
+        4'b1010: tmp = {1'b0, (~B)};
+        4'b1011: tmp = {1'b0, (A | B)};
+        4'b1100: tmp = {1'b0, (A ^ B)};
+        4'b1101: tmp = {1'b0, ~(A & B)};
+        4'b1110: tmp = {1'b0, A};
+        4'b1111: tmp = {1'b0, B};
+        default: tmp = '0;
+      endcase
+
+      O    = tmp[5:0];
+      Cout = tmp[6];
+      Zero = (tmp[5:0] == '0);
     end
   end
+
 endmodule
